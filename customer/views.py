03ad56628f0f330670eb97412ddce312a597fe7c
sadi22 <sadimohammad7@gmail.com>
2016-07-30 12:12:18 +0600

    'Reformat'

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': 'Hello saddi '}
+    return render(request, 'customers/signup.html', context)
