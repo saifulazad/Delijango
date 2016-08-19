663e8a533dcd037c85f07bae6515d9067a1d19ec
sadi22 <sadimohammad7@gmail.com>
2016-08-19 23:56:36 +0600

    add search page

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # ex: /customers
     url(r'^(?P<product_id>[0-9]+)$', views.product_detail, name='product_detail'),
+    url(r'^search/$', views.search, name='product_search'),
 ]
