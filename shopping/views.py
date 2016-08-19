78529a7621d84c02e69029269f8b1dad6f8c2337
sadi22 <sadimohammad7@gmail.com>
2016-08-19 11:51:11 +0600

    fix

--- shopping/views.py
+++ shopping/views.py
@@ -1,9 +1,7 @@
 from carton.cart import Cart
 import simplejson as json
-from django.core import serializers
 from django.http import HttpResponse
 from django.shortcuts import render
-# import json
 from products.models import Product
 
 
@@ -11,15 +9,10 @@ def add(request):
     cart = Cart(request.session)
     product = Product.objects.get(id=request.POST.get('id'))
     cart.add(product, price=product.price, quantity=request.POST.get('qty'))
-    recent_product = {
-        'name': product.productName,
-        'image': product.image.url,
-        'price': product.price,
-        'total': cart.total,
-        'items': cart.count,
-    }
+    recent_product = dict(name=product.productName, image=product.image.url, price=product.price, total=cart.total,
+                          items=cart.count)
     return HttpResponse(
-        json.dumps(recent_product,use_decimal=True),
+        json.dumps(recent_product, use_decimal=True),
         content_type="application/json"
     )
     # return HttpResponse(recent_product)
